cc08e92e66ef9beb084b9f0ccc652834c7edc1b2
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-05 15:02:12 +0800

    Add files via upload

---
+++ Number14.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Number14 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Number14
+{
+    public static void main(String[] args){
+        int[] arr = {1, 2, 3, 4, 5,};
+        System.out.println("Array in reverse: ");
+        for (int i = arr.length - 1; i >=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
