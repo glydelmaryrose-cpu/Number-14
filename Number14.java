afb6204c31c98ba863611293effaa524b332f277
Glydel Mary Rose Visto <glydelmaryrose@gmail.com>
2025-12-07 03:30:52 +0800

    Update Number14.java

--- Number14.java
+++ Number14.java
@@ -9,6 +9,12 @@ public class Number14
 {
     public static void main(String[] args){
         int[] arr = {1, 2, 3, 4, 5,};
+        
+        System.out.println("Original array: ");
+        for (int x : arr) 
+            System.out.print(x + " ");
+        
+        System.out.println(" ");
         System.out.println("Array in reverse: ");
         for (int i = arr.length - 1; i >=0; i--){
             System.out.print(arr[i] + " ");
